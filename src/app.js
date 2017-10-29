62b881fdb170c36a66eac24392ed8703e6f4df0d
Gawande, Arpit <Arpit.Gawande@in.tesco.com>
2017-10-29 15:40:13 +0530

    Added homepage route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import serverRenderer from './serverRenderer';
 
 const app = express();
 const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+  const renderer = new serverRenderer();
+  renderer.route(req, res);
+})
 
 app.get('/_status', (req, res) => {
   res.send('ok');
